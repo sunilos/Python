6432b4d28f9d0c264cff34d40e409994265aa625
Anjali Gupta <anjali.gupta@nenosystems.com>
2019-06-29 10:31:10 +0530

    python Program

--- Threading/RaceCondition.py
+++ Threading/RaceCondition.py
@@ -1,5 +1,6 @@
-# Example of Creating Synchronise thread  using join method
-#
+# Example of Creating Synchronise thread  
+# In this Program you will see first time it print Ram and Shyam 
+# And then  after it will print Ram and Shyam  continously and at last it print Bye by main thread
 # @author SunilOS  
 # @version 1.0
 # @Copyright (c) SunilOS  
@@ -7,30 +8,40 @@
 #
 
 
-from time import sleep
-from threading import *
+import threading
+import time
+ 
+class MyThread(threading.Thread):
+    def __init__(self,threadId, name,counter):
+        threading.Thread.__init__(self)
+        self.threadId = threadId
+        self.name = name
+        self.counter = counter
 
-#Create a class by inheriting Thread class
-class Hello(Thread):
-   def run(self):
-    for i in range(20):
-        print("Ram")
-        
-#Create a class by inheriting Thread class
-class Hi(Thread):
-  def run(self):
-   for i in range(20):
-         print("Shyam")
-        
-#create thread instances 
-t1 = Hello()
-t2 = Hi()
+    
+
+    def run(self):
+       print("Hello  " + self.name) 
+       #Get Lock to synchronise Thread
+       threadLock.acquire()
+       get(self.name,self.counter,3)
+        #Free The Lock to release Next Thread
+       threadLock.release()
+
+
+def get(threadName,delay,counter):
+    while counter:
+     time.sleep(delay)
+     print(threadName)
+     counter -= 1
+threadLock = threading.Lock()
+threads = []
+
+t1 =  MyThread(1,"Ram",1)
+t2 = MyThread(2,"Shyam",2)
 
 t1.start()
-sleep(0.2)
 t2.start()
-
 t1.join()
 t2.join()
-
 print("Bye")
