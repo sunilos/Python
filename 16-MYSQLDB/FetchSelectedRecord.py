049d1ea920f6080cd02865f18180ebdf85fc9750
Anjali Gupta <anjali.gupta@nenosystems.com>
2019-08-02 19:32:24 +0530

    Modified Ftech Selectd Records

--- 16-MYSQLDB/FetchSelectedRecord.py
+++ 16-MYSQLDB/FetchSelectedRecord.py
@@ -16,10 +16,9 @@ conn = connection.MySQLConnection(user='root',
     charset='utf8', 
     database='testdb')
 
-sql_fetch = "SELECT  RollNo FROM student WHERE RollNo Like '%cs%'"
+sql_fetch = "SELECT RollNo FROM student WHERE RollNo Like '%cs%'"
 my_cursor = conn.cursor()
-my_cursor.execute(sql_fetch)
-my_result = my_cursor.fetchall()
+my_result = my_cursor.fetchall() #  This will fetch the records  where the rollno  contains the word "cs"
 
 for x in my_result:
   print(x)
