5fff8fd59b4abaadc783be234f24fedeefa47002
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-16 11:51:55 +0530

    Update ReadTextFile.py

--- 11-IO/ReadTextFile.py
+++ 11-IO/ReadTextFile.py
@@ -1,7 +1,9 @@
 #Read a file 
 def readFile():
-    file = open("UserModel.py") #Open a file
+    file = open("abc.txt") #Open a file
     text = file.read() # Read all data 
     print(text) 
     file.close() # Close a file
-    return
+    
+readFile()    
+ 
