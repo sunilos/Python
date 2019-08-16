cfccb274f7ccc23a3c741de071a3bd27bb741c8e
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-16 11:56:51 +0530

    Update and rename TestFileIO.py to fileInfo.py

---
+++ 11-IO/fileInfo.py
@@ -0,0 +1,18 @@
+# Example of File I/o  
+#
+# @author SunilOS  
+# @version 1.0
+# @Copyright (c) SunilOS  
+# @Url www.SunilOs.com
+#
+
+#Prints properies of a file
+def fileInfo():
+    fo = open("Test.py", "wb") 
+    print ("File Name: ", fo.name)
+    print ("Mode of Opening: ", fo.mode)
+    print ("Is Closed: ", fo.closed)
+    return
+
+
+fileInfo()
