b1e0c02919e350dbddfccaac3dd6f852db0b43ba
SunilOS <sunilos007@gmail.com>
2019-08-16 18:44:10 +0530

    Update SendTextEmail.py

--- 15-Email/SendTextEmail.py
+++ 15-Email/SendTextEmail.py
@@ -1,1 +1,20 @@
-SendTextEmail
+import smtplib
+
+gmail_user = 'you@gmail.com'
+gmail_password = 'P@ssword!'
+
+sent_from = gmail_user
+to = ['me@gmail.com', 'bill@gmail.com']
+subject = 'OMG Super Important Message'
+body = 'Hi, What is going on'
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+    print 'Email sent!'
+
+except:
+    print 'Something went wrong...'
