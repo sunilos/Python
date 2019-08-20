44d4fdc38c4e54f186170ab48bd4a5808e9b1e77
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-20 14:48:56 +0530

    Update CurrentTime.py

--- 03-Datetime/CurrentTime.py
+++ 03-Datetime/CurrentTime.py
@@ -2,3 +2,6 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
+print("Current Hours = ",now.hour)
+print("Current Minutes = ",now.minute)
+print("Current Seconds = ", now.second)
