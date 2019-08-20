1ad199027d1a1c2fb407740d8456abb4bbf318d8
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-20 14:59:18 +0530

    Update CalculateAge.py

--- 03-Datetime/CalculateAge.py
+++ 03-Datetime/CalculateAge.py
@@ -5,8 +5,8 @@ def calculateAge(birthDate):
     age = today.year - birthDate.year - ((today.month, today.day) <(birthDate.month, birthDate.day)) 
     return age 
       
-year=int(input("Year your Birth Year = "))
-month=int(input("Year your Birth Month = "))
-day=int(input("Year your Birth Day = "))
+year=int(input("Enter your Birth Year = "))
+month=int(input("Enter your Birth Month = "))
+day=int(input("Enter your Birth Day = "))
 age=calculateAge(date(year, month, day))
 print(age, "years")
