1ac9693add5a4952ef768dc84d39f5e5b4d51dac
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-20 14:15:04 +0530

    Update CurrentDayMonthYear.py

--- 03-Datetime/CurrentDayMonthYear.py
+++ 03-Datetime/CurrentDayMonthYear.py
@@ -1,5 +1,16 @@
-from datetime import date
+from datetime import date,timedelta
 currentStatus= date.today()
 print("Current Date =", currentStatus.day)
 print("Current Month =", currentStatus.month)
 print("Current Year =", currentStatus.year)
+
+#check tomorrow Date
+tomorrowDate=currentStatus+timedelta(days=1)
+print("tomorrow Date = ",tomorrowDate)
+
+#check after tomorrow Date
+aftertomorrow=tomorrowDate+timedelta(days=1)
+print("After tomorrow Date = ",aftertomorrow)
+
+
+
