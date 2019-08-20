277c9fb1bcd20a9ae057ae5613e5de9a7af0be00
sahumayank <42769416+sahumayank@users.noreply.github.com>
2019-08-20 13:38:29 +0530

    Create CurrentDayMonthYear.py

---
+++ 03-Datetime/CurrentDayMonthYear.py
@@ -0,0 +1,5 @@
+from datetime import date
+currentStatus= date.today()
+print("Current Date =", currentStatus.day)
+print("Current Month =", currentStatus.month)
+print("Current Year =", currentStatus.year)
