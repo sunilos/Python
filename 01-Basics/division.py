df45995b7a0d8ee4ab9dfb41a60aeda05aa0454f
mahi0130 <144420768+mahi0130@users.noreply.github.com>
2024-09-14 18:25:32 +0530

    done

---
+++ 01-Basics/division.py
@@ -0,0 +1,10 @@
+# Assigning values to variables
+a = 50  # Dividend
+b = 5   # Divisor
+c = 0   # Initializing variable to store result
+
+# Performing division
+c = a / b  # Dividing a by b
+
+# Printing the result of the division
+print("Division = ", c)
